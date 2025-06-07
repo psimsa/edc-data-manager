c726eebf405b85c2d9e104151a95ce034e7ef32a
Pavel Simsa <pavel@simsa.cz>
2025-06-07 15:08:47 +0200

    WIP

--- src/EdcMgmt.Client/Program.cs
+++ src/EdcMgmt.Client/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 using EdcMgmt.Client;
+using KristofferStrube.Blazor.FileSystemAccess;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents();
 
-await builder.Build().RunAsync();
+builder.Services.AddFileSystemAccessService();
+
+builder.Services.AddLocalization();
+var app = builder.Build();
+
+await app.RunAsync();
